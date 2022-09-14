da3bc1408afd4ffa9898d34c792edb6ba956afe3
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-09-14 15:53:56 +0900

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -31,15 +31,15 @@ END=datetime(2022,5,9)   #마지막달 다음달 1일
 
 st.sidebar.subheader("Filter Displayed Accounts")
 
-  accounts = list(START,END)
-  account_selections = st.sidebar.multiselect(
-      "Select Accounts to View", options=accounts, default=accounts
-  )
-  st.sidebar.subheader("Filter Displayed Tickers")
+accounts = list(START,END)
+account_selections = st.sidebar.multiselect(
+    "Select Accounts to View", options=accounts, default=accounts
+)
+st.sidebar.subheader("Filter Displayed Tickers")
 
-  symbols = list(START,END)
-  symbol_selections = st.sidebar.multiselect(
-      "Select Ticker Symbols to View", options=symbols, default=symbols
-  )
+symbols = list(START,END)
+symbol_selections = st.sidebar.multiselect(
+    "Select Ticker Symbols to View", options=symbols, default=symbols
+)
 
   st.subheader("Selected Account and Ticker Data")
